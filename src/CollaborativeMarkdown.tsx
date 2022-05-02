96f96dc6f573cc29910c8c9a4f95676ae90b34ca
Rodrigo Liberoff Vázquez <rliberoff@encamina.com>
2022-05-02 20:47:20 +0200

    Removed comments to test deployment again...

--- src/CollaborativeMarkdown.tsx
+++ src/CollaborativeMarkdown.tsx
@@ -3,9 +3,6 @@ import { ISharedStringHelperTextChangedEventArgs, SharedStringHelper } from '@fl
 import MDEditor from '@uiw/react-md-editor';
 
 interface ICollaborativeTextAreaProps {
-    /**
-     * The SharedString that will store the text from the textarea.
-     */
     sharedStringHelper: SharedStringHelper;
     textAreaSuffixId: string;
 }
@@ -32,20 +29,12 @@ export const CollaborativeMarkdown = (props: ICollaborativeTextAreaProps) => {
         textareaElement.selectionEnd = newEnd;
     };
 
+    // Handles changes from remote...
     React.useEffect(() => {
-        /**
-         * There's been a change to the SharedString's data.  This means the most recent state of the text
-         * is in the SharedString, and we need to
-         * 1. Store the text state in React
-         * 2. If the change came from a remote source, it may have moved our selection.  Compute it, update
-         *    the textarea, and store it in React
-         */
         const handleTextChanged = (event: ISharedStringHelperTextChangedEventArgs) => {
             const newText = sharedStringHelper.getText();
             setText(newText);
 
-            // If the event was our own then the caret will already be in the new location.
-            // Otherwise, transform our selection position based on the change.
             if (!event.isLocal) {
                 const newSelectionStart = event.transformPosition(selectionStartRef.current);
                 const newSelectionEnd = event.transformPosition(selectionEndRef.current);
@@ -60,25 +49,19 @@ export const CollaborativeMarkdown = (props: ICollaborativeTextAreaProps) => {
         };
     });
 
+    // Handles changes from local...
     const handleChange = (ev: React.FormEvent<HTMLTextAreaElement>) => {
         const textareaElement = ev.target as HTMLTextAreaElement;
         const newText = textareaElement.value;
-        // After a change to the textarea content we assume the selection is gone (just a caret)
-        // This is a bad assumption (e.g. performing undo will select the re-added content).
         const newCaretPosition = textareaElement.selectionStart;
 
-        // Next get and stash the old React state
         const oldText = text;
         const oldSelectionStart = selectionStartRef.current;
         const oldSelectionEnd = selectionEndRef.current;
 
-        // Next update the React state with the values from the textarea
         storeSelectionInReact();
         setText(newText);
 
-        // Finally update the SharedString with the values after deducing what type of change it was.
-        // If the caret moves to the right of the prior left bound of the selection, we assume an insert occurred
-        // This is also a bad assumption, in the undo case.
         const isTextInserted = newCaretPosition - oldSelectionStart > 0;
         if (isTextInserted) {
             const insertedText = newText.substring(oldSelectionStart, newCaretPosition);
@@ -89,7 +72,6 @@ export const CollaborativeMarkdown = (props: ICollaborativeTextAreaProps) => {
                 sharedStringHelper.replaceText(insertedText, oldSelectionStart, oldSelectionEnd);
             }
         } else {
-            // Text was removed
             const charactersDeleted = oldText.length - newText.length;
             sharedStringHelper.removeText(newCaretPosition, newCaretPosition + charactersDeleted);
         }
